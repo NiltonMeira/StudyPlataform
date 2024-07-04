be55633dc1da038d3bd1cc7573f1105b07f3788a
NiltonMeira <niltonprudys@gmail@gmail.com>
2024-07-04 15:56:28 -0300

    feat: finishing the submit logic

--- frontend/src/components/Form/Form.tsx
+++ frontend/src/components/Form/Form.tsx
@@ -1,19 +1,35 @@
+import { SyntheticEvent, useState } from "react";
 import { StyledForm } from "./style"
 import { MainContainer } from "./style"
 import { StyledInput } from "./style";
 import { StyledButton } from "./style";
 
 export const Form = () => {
+
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event: SyntheticEvent<HTMLFormElement, SubmitEvent>) =>{
+        event.preventDefault()
+        alert(`The name you entered was: ${name} and the password was ${password}`)
+
+    }
+
     return (
         <>
-
             <MainContainer>
-                <StyledForm customMargin={30}>
+                <StyledForm customMargin={30} onSubmit={handleSubmit}>
                     <p>User</p>
-                    <StyledInput placeholder=""></StyledInput>
+                    <StyledInput
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
                     <p>Password</p>
-                    <StyledInput placeholder=""></StyledInput>
-                    <StyledButton>Login</StyledButton>
+                    <StyledInput
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <StyledButton type="submit">Login</StyledButton>
                 </StyledForm>
             </MainContainer>
 
